de3f7674dc46575ebb041d7f8fa28c35096b1292
natechaseh <nathanielhajian@gmail.com>
2016-02-24 19:00:37 -0800

    Initial Commit

---
+++ Youtube LabTests/Youtube_LabTests.swift
@@ -0,0 +1,36 @@
+//
+//  Youtube_LabTests.swift
+//  Youtube LabTests
+//
+//  Created by Nathaniel Hajian on 2/24/16.
+//  Copyright © 2016 Nathaniel Hajian. All rights reserved.
+//
+
+import XCTest
+@testable import Youtube_Lab
+
+class Youtube_LabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
